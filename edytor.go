d9edd90cd171a7a452176a3e4e64e2cab849cfa9
Mirosław Waldowski <mwa@amu.edu.pl>
2018-11-19 23:08:16 +0100

    zarys _menu

--- edytor.go
+++ edytor.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
 )
 
+//struktura edytor, która opisuje co bedzie zaiwerało góówne okno
 type Edytor struct {
 	widgets.QMainWindow
 
-	oknoEdytor *widgets.QTextEdit
+	oknoEdytor *widgets.QTextEdit //okno tekstowe
 }
 
+// funkcja inicjująca
 func initEdytor() *Edytor {
+	//tworzy strukturę okno typu Edytor
 	var okno = NewEdytor(nil, 0)
-
+	// w strukturze okno inicjuje funkcją nowe oknoEdytor
 	okno.oknoEdytor = widgets.NewQTextEdit(okno)
+	//utworzony oknoEdytor w strukturze okno typu Edytor przypisuje do zmiennej oknoE
 	var oknoE = okno.oknoEdytor
-
+	//ustawia oknoE centralnie w okno
 	okno.SetCentralWidget(oknoE)
 
+	okno.SetToolButtonStyle(core.Qt__ToolButtonFollowStyle)
+	//metody tworzące menu plik i edytuj
+	okno.setupPlik()
+	okno.setupEdytuj()
 	return okno
+}
+
+func (e *Edytor) setupPlik() {
+	//tworzy menu plik
+	var menu = e.MenuBar().AddMenu2("&Plik")
+
+	var nowyIkona = gui.QIcon_FromTheme2("nowy", gui.NewQIcon5(":/qml/ikony/new.png"))
+	var a = menu.AddAction2(nowyIkona, "&Nowy")
+	a.SetShortcuts2(gui.QKeySequence__New)
+	menu.AddSeparator()
+
+}
+
+func (e *Edytor) setupEdytuj() {
+	//tworzy menu edytuj
+	var menu = e.MenuBar().AddMenu2("Edytuj")
+	var wytnijIkona = gui.QIcon_FromTheme2("wytnij", gui.NewQIcon5(":/qml/ikony/cut.png"))
+	var a = menu.AddAction2(wytnijIkona, "&Wytnij")
+	a.SetShortcuts2(gui.QKeySequence__Cut)
+	menu.AddSeparator()
+
 }
--- main.go
+++ main.go
@@ -7,18 +7,25 @@ import (
 	"github.com/therecipe/qt/widgets"
 )
 
+//deklaruję główną aplikację jako strukturę typu QApplication
 var aplikacja *widgets.QApplication
 
 func main() {
+
+	//funkcja tworzy aplikację typu QApplication
 	aplikacja = widgets.NewQApplication(len(os.Args), os.Args)
 
+	//funkcje wprowadzają informacje do aplikacji
 	core.QCoreApplication_SetApplicationName("MirPad")
 	core.QCoreApplication_SetApplicationVersion("1.0")
 
+	//deklaruję główne okno aplikacji, które jest tworzone przez funkcję inicjującą
+	//jako struktura typuEedytor
 	var glowneOkno = initEdytor()
-
+	//ustawia rozmiar
 	glowneOkno.SetMinimumSize2(600, 450)
-
+	//pokazuje okno
 	glowneOkno.Show()
+	//uruchamia stworzoną aplikacje
 	aplikacja.Exec()
 }
