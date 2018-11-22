8d8c6cecd6fdcd27a835c3f42ba49aff9a412ea2
Mirosław Waldowski <mwa@amu.edu.pl>
2018-11-23 00:22:29 +0100

    dodano_zoom_i_load

--- edytor.go
+++ edytor.go
@@ -11,6 +11,7 @@ type Edytor struct {
 	widgets.QMainWindow // elementy z "klasy" QMainWindow
 
 	oknoEdytor *widgets.QTextEdit //okno tekstowe
+	nazwaPliku string
 }
 
 // funkcja inicjująca
@@ -47,6 +48,7 @@ func (e *Edytor) setupPlik() {
 	a = menu.AddAction2(otworzIkona, "Otwórz")
 	a.SetShortcuts2(gui.QKeySequence__Open)
 	pasek.QWidget.AddAction(a)
+	a.ConnectTriggered(func(checked bool) { e.Otworz() })
 	//
 	var zapiszIkona = gui.QIcon_FromTheme2("zapisz", gui.NewQIcon5(":/qml/ikony/save.png"))
 	a = menu.AddAction2(zapiszIkona, "Zapisz")
@@ -60,7 +62,8 @@ func (e *Edytor) setupPlik() {
 	//
 	var wyjscieIkona = gui.QIcon_FromTheme2("wyjście", gui.NewQIcon5(":/qml/ikony/close.png"))
 	a = menu.AddAction2(wyjscieIkona, "Wyjście")
-	a.SetShortcuts2(gui.QKeySequence__Quit) //sprawdzić skrót
+	a.SetShortcuts2(gui.QKeySequence__Cancel) //sprawdzić skrót
+	a.ConnectTriggered(func(checked bool) { e.Close() })
 
 }
 
@@ -74,25 +77,45 @@ func (e *Edytor) setupEdytuj() {
 	var a = menu.AddAction2(wytnijIkona, "Wytnij")
 	a.SetShortcuts2(gui.QKeySequence__Cut)
 	pasek.QWidget.AddAction(a)
+	a.ConnectTriggered(func(checked bool) { e.oknoEdytor.Cut() })
 	//
 	var kopiujIkona = gui.QIcon_FromTheme2("kopiuj", gui.NewQIcon5(":/qml/ikony/copy.png"))
 	a = menu.AddAction2(kopiujIkona, "Kopiuj")
 	a.SetShortcuts2(gui.QKeySequence__Copy)
 	pasek.QWidget.AddAction(a)
+	a.ConnectTriggered(func(checked bool) { e.oknoEdytor.Copy() })
 	//
 	var wklejIkona = gui.QIcon_FromTheme2("wklej", gui.NewQIcon5(":/qml/ikony/paste.png"))
 	a = menu.AddAction2(wklejIkona, "Wklej")
 	a.SetShortcuts2(gui.QKeySequence__Paste)
 	pasek.QWidget.AddAction(a)
+	a.ConnectTriggered(func(checked bool) { e.oknoEdytor.Paste() })
+	//
+	menu.AddSeparator()
 	//
 	var cofnijIkona = gui.QIcon_FromTheme2("cofnij", gui.NewQIcon5(":/qml/ikony/undo-arrow.png"))
 	a = menu.AddAction2(cofnijIkona, "Cofnij")
 	a.SetShortcuts2(gui.QKeySequence__Undo)
 	pasek.QWidget.AddAction(a)
+	a.ConnectTriggered(func(checked bool) { e.oknoEdytor.Undo() })
 	//
 	var ponowIkona = gui.QIcon_FromTheme2("ponów", gui.NewQIcon5(":/qml/ikony/redo-arrow.png"))
 	a = menu.AddAction2(ponowIkona, "Ponów")
 	a.SetShortcuts2(gui.QKeySequence__Redo)
 	pasek.QWidget.AddAction(a)
-
+	a.ConnectTriggered(func(checked bool) { e.oknoEdytor.Redo() })
+	//
+	menu.AddSeparator()
+	//
+	var powiekszIkona = gui.QIcon_FromTheme2("powiększ", gui.NewQIcon5(":/qml/ikony/zoom-in.png"))
+	a = menu.AddAction2(powiekszIkona, "Powiększ")
+	a.SetShortcuts2(gui.QKeySequence__ZoomIn)
+	pasek.QWidget.AddAction(a)
+	a.ConnectTriggered(func(checked bool) { e.oknoEdytor.ZoomIn(2) })
+	//
+	var pomniejszIkona = gui.QIcon_FromTheme2("pomniejsz", gui.NewQIcon5(":/qml/ikony/zoom-out.png"))
+	a = menu.AddAction2(pomniejszIkona, "Pomniejsz")
+	a.SetShortcuts2(gui.QKeySequence__ZoomOut)
+	pasek.QWidget.AddAction(a)
+	a.ConnectTriggered(func(checked bool) { e.oknoEdytor.ZoomOut(2) })
 }
--- funkcje.go
+++ funkcje.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
+)
+
 func (e *Edytor) Nowy() {
 	e.oknoEdytor.Clear()
-	// dodać metodęsprawdzającą czy aktywny plik był modyfikowany
+	// dodać metodę sprawdzającą czy aktywny plik był modyfikowany
+}
+
+func (e *Edytor) Otworz() {
+	var oknoDialog = widgets.NewQFileDialog2(e, "Otwórz plik", "", "pliki tekstowe (*.txt)")
+	oknoDialog.SetAcceptMode(widgets.QFileDialog__AcceptOpen)
+	oknoDialog.SetFileMode(widgets.QFileDialog__ExistingFile)
+
+	oknoDialog.Exec()
+
+	var plikNazwa = oknoDialog.SelectedFiles()[0] //wybrany plik w oknie
+
+	if core.QFile_Exists(plikNazwa) { //sprawdza czy plik istnieje
+
+		e.oknoEdytor.SetPlainText("wprowadzić zamiast tego tekst z pliku")
+	}
+
 }
